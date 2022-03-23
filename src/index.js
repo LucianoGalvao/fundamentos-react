cf849261fb4d6f76d22c536f8874d1abaf636615
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-03-22 22:27:03 -0300

    First react component create, app default

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from './App'
 
-ReactDom.render(<h1>Olá mundo!</h1>, document.getElementById('root'));
+ReactDom.render(
+  <App />,
+  document.getElementById('root')
+);

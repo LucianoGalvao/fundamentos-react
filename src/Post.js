4ec3725576d3fa28af143422a22e9026a8c809fc
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-03-30 20:29:54 -0300

    Blog Post Create

---
+++ src/Post.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import PostHeader from './PostHeader';
+
+export default function Post(props) {
+  return (
+    <>
+      <article>
+        <PostHeader
+          onRemove={props.onRemove}
+          post={{
+            id: props.post.id,
+            title: props.post.title,
+            read: props.post.read,
+          }}
+        />
+        <br />
+        <small>{props.post.subtitle}</small>
+        <br />
+        Média: {props.post.likes / 2}
+      </article>
+      <br />
+    </>
+  );
+}
+
+Post.propTypes = {
+  onRemove: propTypes.func.isRequired,
+  post: propTypes.shape({
+    id: propTypes.number.isRequired,
+    title: propTypes.string.isRequired,
+    subtitle: propTypes.string.isRequired,
+    likes: propTypes.number.isRequired,
+    read: propTypes.bool.isRequired,
+  }).isRequired,
+};

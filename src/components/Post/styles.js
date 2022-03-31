498823080550f4f04125cb2bfac5026c9663af1c
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-03-31 20:44:40 -0300

    Post title styled component

--- src/components/Post/styles.js
+++ src/components/Post/styles.js
@@ -1,10 +1,22 @@
 import styled from 'styled-components';
 
+import PostTile from '../Title';
+
+export const Container = styled.article`
+  margin-bottom: 24px;
+  opacity: ${(props) => (props.removed ? 0.5 : 1)};
+`;
+
 export const Subtitle = styled.small`
   display: block;
-`
+`;
 
 export const Rate = styled.span`
   font-size: 10px;
   opacity: 0.7;
-`
+`;
+
+export const Title = styled(PostTile)`
+  color: #fff;
+  text-decoration: ${(props) => (props.read ? 'line-through' : 'none')};
+`;

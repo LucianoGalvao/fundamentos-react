712ddd38b562ca55f30339ad69c388bfd817f07c
Luciano Galvão <luciano_sdmf@hotmail.com>
2022-04-07 21:29:41 -0300

    Routes add

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
+import { Nav } from './styles';
+
+import { Link, BrowserRouter } from 'react-router-dom';
+
 import Header from '../Header';
-import PostsList from '../PostsList';
 import Footer from '../Footer';
+
+import Routes from '../../routes';
 
 export default function Layout({ onToggleTheme, selectedTheme }) {
   return (
-    <>
+    <BrowserRouter>
       <Header onToggleTheme={onToggleTheme} selectedTheme={selectedTheme} />
-      <PostsList />
+      <Nav>
+        <Link to='/'>Home</Link>
+        <Link to='/posts'>Posts</Link>
+        <Link to='/posts/id'>Post</Link>
+      </Nav>
+      <Routes />
       <Footer onToggleTheme={onToggleTheme} selectedTheme={selectedTheme} />
-    </>
+    </BrowserRouter>
   );
 }
